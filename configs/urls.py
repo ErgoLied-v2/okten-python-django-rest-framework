aa44a697c675745292a8a268637e59f98da5eee8
ErgoLied-v2 <nakamuraritsu@gmail.com>
2024-07-24 10:05:09 +0200

    crud done

--- configs/urls.py
+++ configs/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from first.views import TestView
+from first.views import CarListCreateView, CarRetrieveUpdateDestroyView
 urlpatterns = [
-    path('test', TestView.as_view()),
+    path('cars', CarListCreateView.as_view()),
+    path('cars/<int:pk>', CarRetrieveUpdateDestroyView.as_view()),
 ]
--- first/views.py
+++ first/views.py
@@ -1,19 +1,58 @@
+from django.forms import model_to_dict
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from first.models import CarModel
 
-class TestView(APIView):
+
+class CarListCreateView(APIView):
     def get(self, *args, **kwargs):
-        return Response('hello from get')
+        cars = CarModel.objects.all()
+        res = [model_to_dict(car) for car in cars]
+        return Response(res, status.HTTP_200_OK)
 
     def post(self, *args, **kwargs):
-        return Response('hello from post')
+        data = self.request.data
+        car = CarModel.objects.create(**data)
+        res = model_to_dict(car)
+        return Response(res, status.HTTP_201_CREATED)
+
+
+class CarRetrieveUpdateDestroyView(APIView):
+    def get(self, *args, **kwargs):
+        pk = kwargs['pk']
+        try:
+            car = CarModel.objects.get(pk=pk)
+        except CarModel.DoesNotExist:
+            return Response('not found', status.HTTP_404_NOT_FOUND)
+
+        return Response(model_to_dict(car), status.HTTP_200_OK)
 
     def put(self, *args, **kwargs):
-        return Response('hello from put')
+        pk = kwargs['pk']
+        data = self.request.data
+        try:
+            car = CarModel.objects.get(pk=pk)
 
-    def patch(self, *args, **kwargs):
-        return Response('hello from patch')
+        except CarModel.DoesNotExist:
+            return Response('not found', status.HTTP_404_NOT_FOUND)
+
+        car.brand = data['brand']
+        car.price = data['price']
+        car.year = data['year']
+        car.save()
+        return Response(model_to_dict(car), status.HTTP_200_OK)
+
+    # def patch(self, *args, **kwargs):
+    #     return Response('hello from patch')
 
     def delete(self, *args, **kwargs):
-        return Response('hello from delete')
+        pk = kwargs['pk']
+        try:
+            car = CarModel.objects.get(pk=pk)
+            car.delete()
+        except CarModel.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response('deleted', status.HTTP_204_NO_CONTENT)
