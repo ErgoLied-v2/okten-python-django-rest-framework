1f42c469fc831087cf5acc49e8f099514ed73977
ErgoLied-v2 <nakamuraritsu@gmail.com>
2024-08-03 19:41:25 +0200

    hw: filters

--- first/filter.py
+++ first/filter.py
@@ -10,10 +10,45 @@ def car_filter(query: QueryDict) -> QuerySet:
 
     for key, value in query.items():
         match key:
+            case 'id_gt':
+                queryset = queryset.filter(id__gt=value)
+            case 'id_gte':
+                queryset = queryset.filter(id__gte=value)
+            case 'id_lt':
+                queryset = queryset.filter(id__lt=value)
+            case 'id_lte':
+                queryset = queryset.filter(id__lte=value)
+
+            case 'year_gt':
+                queryset = queryset.filter(year__gt=value)
+            case 'year_gte':
+                queryset = queryset.filter(year__gte=value)
+            case 'year_lt':
+                queryset = queryset.filter(year__lt=value)
+            case 'year_lte':
+                queryset = queryset.filter(year__lte=value)
+
             case 'price_gt':
                 queryset = queryset.filter(price__gt=value)
+            case 'price_gte':
+                queryset = queryset.filter(price__gte=value)
             case 'price_lt':
                 queryset = queryset.filter(price__lt=value)
+            case 'price_lte':
+                queryset = queryset.filter(price__lte=value)
+
+            case 'brand_startswith':
+                queryset = queryset.filter(brand__startswith=value)
+            case 'brand_endswith':
+                queryset = queryset.filter(brand__endswith=value)
+            case 'brand_contains':
+                queryset = queryset.filter(brand__contains=value)
+
+            case 'sort_by':
+                queryset = queryset.order_by(f'{value}')
+            case 'sort_by_desc':
+                queryset = queryset.order_by(f'-{value}')
+
             case _:
                 raise ValidationError(f"Filter {key} not supported")
 
